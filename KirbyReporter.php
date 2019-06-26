77aeeb40f40a9f044e7e937b21bc771ad9ed5f74
Newname <steffen.giers@gmail.com>
2019-06-26 16:56:59 +0200

    refactor: optimize api design

--- KirbyReporter.php
+++ KirbyReporter.php
@@ -1,75 +1,74 @@
 <?php
 
-use Gearsdigital\KirbyReporter\ResponseError;
-use Gearsdigital\KirbyReporter\Platform;
-use Gearsdigital\KirbyReporter\Gateway;
+use KirbyReporter\Client\CreateClient;
+use KirbyReporter\Client\CreateVendor;
+use KirbyReporter\Client\ErrorResponse;
 
 $pluginState = option('kirby-reporter.disabled');
 if ($pluginState || is_null($pluginState)) {
     return false;
 }
+Kirby::plugin(
+    'gearsdigital/kirby-reporter',
+    [
+        'api'          => [
+            'routes' => [
+                [
+                    'pattern' => 'kirby-reporter',
+                    'method'  => 'post',
+                    'action'  => function () {
+                        $url = option('kirby-reporter.repository');
+                        $token = option('kirby-reporter.token');
+                        try {
+                            $vendor = new CreateVendor($url);
+                            $client = new CreateClient($vendor, $token);
+                            $response = $client->api->createIssue(kirby()->api()->requestBody());
 
-Kirby::plugin('gearsdigital/kirby-reporter', [
-    'api' => [
-        'routes' => [
-            [
-                'pattern' => 'kirby-reporter',
-                'method' => 'post',
-                'action' => function () {
-                    $url = option('kirby-reporter.repository');
-                    $token = option('kirby-reporter.token');
-                    $platform = new Platform($url, $token);
-                    $gateway = new Gateway($platform);
+                            return json_encode($response);
+                        } catch (Exception $e) {
+                            $errorResponse = new ErrorResponse($e);
 
-                    try {
-                        $response = $gateway->api->createIssue(kirby()->api()->requestBody());
-                        return json_encode($response);
-                    } catch (Exception $e) {
-                        $errorResponse = new ResponseError();
-                        $errorResponse->setStatus($e->getCode());
-                        $errorResponse->setPlatform($platform->platform);
-                        $errorResponse->setRepo($platform->owner.DS.$platform->repository);
-
-                        return json_encode($errorResponse);
-                    }
-                }
-            ]
-        ]
-    ],
-    'translations' => [
-        'en' => [
-            'view.issue-tracker' => 'New Issue',
-            'reporter.headline' => 'New Issue',
-            'reporter.description' => 'This is the place to report things that need to be improved or solved. Issues can be bugs, tasks or ideas to be discussed.',
-            'reporter.form.field.title' => 'Title',
-            'reporter.form.success' => 'Your problem has been reported successfully and is handled under case number: {issueLink}',
-            'reporter.form.issue.link' => '<a href="{issueLink}">{issueId}</a>',
-            'reporter.form.button.save' => 'Report Issue',
-            'reporter.form.field.description' => 'Description',
-            'reporter.form.field.description.help' => 'Please be as precise as possible :)',
-            'reporter.form.error.title' => 'You need to add at least a title.',
-            'reporter.form.error.authFailed' => 'Authentication failed. Please check your "Personal Access Token".',
-            'reporter.form.error.repoNotFound' => 'Repository not found.',
-            'reporter.form.error.optionNotFound.url' => 'Option "kirby-reporter.repository" not defined.',
-            'reporter.form.error.optionNotFound.token' => 'Option "kirby-reporter.token" not defined.',
-            'reporter.form.error.platform.unsupported' => 'Your Platform is currently not supported.'
+                            return json_encode($errorResponse);
+                        }
+                    },
+                ],
+            ],
         ],
-        'de' => [
-            'view.issue-tracker' => 'Fehler Melden',
-            'reporter.headline' => 'Fehler Melden',
-            'reporter.description' => 'Hier können Dinge gemeldet werden die verbessert oder behoben werden müssen. Das können Fehler, Aufgaben oder Ideen sein.',
-            'reporter.form.field.title' => 'Titel',
-            'reporter.form.success' => 'Ihr Bericht wurde erfolgreich übertragen und wird unter der Fallnummer {issueLink} behandelt.',
-            'reporter.form.issue.link' => '<a href="{issueLink}">{issueId}</a>',
-            'reporter.form.button.save' => 'Fehler melden',
-            'reporter.form.field.description' => 'Beschreibung',
-            'reporter.form.field.description.help' => 'Bitte beschreiben Sie den Fehler so genau wie möglich :)',
-            'reporter.form.error.title' => 'Es muss mindestens ein Titel eingegeben werden.',
-            'reporter.form.error.authFailed' => 'Anmeldung Fehlgeschlagen. Bitte prüfen Sie den "Personal Access Token".',
-            'reporter.form.error.repoNotFound' => 'Das Repository wurde nicht gefunden.',
-            'reporter.form.error.optionNotFound.url' => 'Die Option "kirby-reporter.repository" ist nicht vorhanden',
-            'reporter.form.error.optionNotFound.token' => 'Option "kirby-reporter.token" ist nicht vorhanden.',
-            'reporter.form.error.platform.unsupported' => 'Die Platform wird zur Zeit nicht Unterstützt.'
-        ]
-    ],
-]);
+        'translations' => [
+            'en' => [
+                'view.issue-tracker'                       => 'New Issue',
+                'reporter.headline'                        => 'New Issue',
+                'reporter.description'                     => 'This is the place to report things that need to be improved or solved. Issues can be bugs, tasks or ideas to be discussed.',
+                'reporter.form.field.title'                => 'Title',
+                'reporter.form.success'                    => 'Your problem has been reported successfully and is handled under case number: {issueLink}',
+                'reporter.form.issue.link'                 => '<a href="{issueLink}">#{issueId}</a>',
+                'reporter.form.button.save'                => 'Report Issue',
+                'reporter.form.field.description'          => 'Description',
+                'reporter.form.field.description.help'     => 'Please be as precise as possible :)',
+                'reporter.form.error.title'                => 'You need to add at least a title.',
+                'reporter.form.error.authFailed'           => 'Authentication failed. Please check your "Personal Access Token".',
+                'reporter.form.error.repoNotFound'         => 'Repository not found.',
+                'reporter.form.error.optionNotFound.url'   => 'Option "kirby-reporter.repository" not defined.',
+                'reporter.form.error.optionNotFound.token' => 'Option "kirby-reporter.token" not defined.',
+                'reporter.form.error.platform.unsupported' => 'Your Platform is currently not supported.',
+            ],
+            'de' => [
+                'view.issue-tracker'                       => 'Fehler Melden',
+                'reporter.headline'                        => 'Fehler Melden',
+                'reporter.description'                     => 'Hier können Dinge gemeldet werden die verbessert oder behoben werden müssen. Das können Fehler, Aufgaben oder Ideen sein.',
+                'reporter.form.field.title'                => 'Titel',
+                'reporter.form.success'                    => 'Ihr Bericht wurde erfolgreich übertragen und wird unter der Fallnummer {issueLink} behandelt.',
+                'reporter.form.issue.link'                 => '<a href="{issueLink}">{issueId}</a>',
+                'reporter.form.button.save'                => 'Fehler melden',
+                'reporter.form.field.description'          => 'Beschreibung',
+                'reporter.form.field.description.help'     => 'Bitte beschreiben Sie den Fehler so genau wie möglich :)',
+                'reporter.form.error.title'                => 'Es muss mindestens ein Titel eingegeben werden.',
+                'reporter.form.error.authFailed'           => 'Anmeldung Fehlgeschlagen. Bitte prüfen Sie den "Personal Access Token".',
+                'reporter.form.error.repoNotFound'         => 'Das Repository wurde nicht gefunden.',
+                'reporter.form.error.optionNotFound.url'   => 'Die Option "kirby-reporter.repository" ist nicht vorhanden',
+                'reporter.form.error.optionNotFound.token' => 'Option "kirby-reporter.token" ist nicht vorhanden.',
+                'reporter.form.error.platform.unsupported' => 'Die Platform wird zur Zeit nicht Unterstützt.',
+            ],
+        ],
+    ]
+);
---
+++ lib/ResponseMapper.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace KirbyReporter\Client;
+
+use Exception;
+use KirbyReporter\Mixins\ArrayTransformator;
+use ReflectionObject;
+use ReflectionProperty;
+
+/**
+ * ResponseMapper
+ *
+ * Public properties define the response object. If you're going to extend the response
+ * object you have to make sure to adapt all implented maps.
+ *
+ * @package KirbyReporter\Client
+ * @author Steffen Giers <steffen.giers@gmail.com>
+ */
+class ResponseMapper
+{
+
+    use ArrayTransformator;
+
+    /**
+     * HTTP Status code
+     *
+     * @var int
+     */
+    public $status;
+
+    /**
+     * @var string | null
+     */
+    public $issueId;
+
+    /**
+     * @var string | null
+     */
+    public $issueUrl;
+
+    /**
+     * @var array
+     */
+    private $map;
+
+    /**
+     * @var array
+     */
+    private $response;
+
+    /**
+     * ResponseMapper constructor.
+     *
+     * @param  array  $map
+     * @param  array  $response
+     *
+     * @throws Exception
+     */
+    public function __construct(array $map, Response $response)
+    {
+        $this->map = $map;
+        $this->response = $response->body;
+        $this->populateProperties();
+        $this->validateProperties();
+        // This needs to happen after all transformations are done because
+        // the status is always populated
+        if (is_numeric($response->status)) {
+            $this->status = $response->status;
+        }
+    }
+
+    /**
+     * Assignes response entry values to matching public properties.
+     *
+     * @return void;
+     */
+    private function populateProperties(): void
+    {
+        $mappedResponse = $this->applyMapToProperties();
+        foreach (get_object_vars($this) as $propertyName => $propertyValue) {
+            $this->$propertyName = $mappedResponse[$propertyName];
+        }
+    }
+
+    /**
+     * Applies all map entries to the reduced response object.
+     *
+     * @return array
+     */
+    private function applyMapToProperties(): array
+    {
+        return $this->transform($this->reduceResponseWithMap(), $this->map);
+    }
+
+    /**
+     * Removes all entries from response array which are not present in map.
+     *
+     * @return array
+     */
+    private function reduceResponseWithMap(): array
+    {
+        return array_intersect_key($this->response, $this->map);
+    }
+
+    /**
+     * Validate that all response properties are mapped.
+     *
+     * The Frontend has specific requirements ss we need to make sure
+     * all entries are mapped correctly :)
+     *
+     * @throws Exception
+     */
+    private function validateProperties(): void
+    {
+        $properties = $this->getPublicProperties();
+        if (in_array(null, $properties)) {
+            throw new Exception('You need to implement all required fields.');
+        }
+    }
+
+    /**
+     * Returns all public properties of this very class.
+     *
+     * @return array
+     */
+    private function getPublicProperties(): array
+    {
+        $reflectionClass = new ReflectionObject($this);
+
+        return $reflectionClass->getProperties(ReflectionProperty::IS_PUBLIC);
+    }
+
+}
