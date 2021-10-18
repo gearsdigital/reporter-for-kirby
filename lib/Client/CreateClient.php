c74a177f83d366126bc85a0a02af83257cffd4a8
gearsdigital <steffen.giers@gmail.com>
2021-10-18 21:09:37 +0200

    fix: add missing arguments

--- lib/Client/CreateClient.php
+++ lib/Client/CreateClient.php
@@ -2,6 +2,8 @@
 
 namespace KirbyReporter\Client;
 
+use QL\UriTemplate\Exception;
+
 /**
  * Create a Client.
  *
@@ -12,7 +14,10 @@ class CreateClient
 {
     public $api;
 
-    public function __construct(CreateVendor $vendor, $token)
+    /**
+     * @throws Exception
+     */
+    public function __construct(CreateVendor $vendor, $token, $user)
     {
         switch ($vendor->name) {
             case 'github':
@@ -22,7 +27,7 @@ class CreateClient
                 $this->api = new Gitlab($vendor, $token);
                 break;
             case 'bitbucket':
-                $this->api = new BitbucketCloud($vendor, $token);
+                $this->api = new BitbucketCloud($vendor, $token, $user);
                 break;
         }
     }
