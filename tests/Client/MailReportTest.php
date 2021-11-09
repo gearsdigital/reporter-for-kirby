a6f495d50063704888653c6263deb93e5bc50f4d
gearsdigital <steffen.giers@gmail.com>
2021-11-09 18:47:16 +0100

    test(mail-report): fix failing test

--- tests/Client/MailReportTest.php
+++ tests/Client/MailReportTest.php
@@ -31,7 +31,7 @@ class MailReportTest extends TestCase
 
     public function test_should_send_mail_with_text_mail_template(): void
     {
-        $expectedArgs = array_merge(['template' => 'reporttext'], $this->defaultEmailArgs);
+        $expectedArgs = array_merge(['template' => 'report.text'], $this->defaultEmailArgs);
         $mock = $this->getMockBuilder(App::class)->onlyMethods(['email'])->getMock();
         $mock->expects($this->exactly(1))->method('email')->with($expectedArgs);
 
