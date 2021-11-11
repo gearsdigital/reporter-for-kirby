aa735f2ea11fb080c184d7e63f3306b48bc57797
gearsdigital <steffen.giers@gmail.com>
2021-11-11 16:43:09 +0100

    fix: remove dev-packages from vendor to decrease package size

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,7 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'claviska' => array($vendorDir . '/claviska/simpleimage/src'),
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
-    'Michelf' => array($vendorDir . '/michelf/php-smartypants'),
 );
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -5,27 +5,13 @@
 $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
-return array(
-    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/reflection-docblock/src', $vendorDir . '/phpdocumentor/type-resolver/src'),
-    'Whoops\\' => array($vendorDir . '/filp/whoops/src/Whoops'),
-    'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
-    'TrueBV\\' => array($vendorDir . '/true/punycode/src'),
-    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
-    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
-    'QL\\UriTemplate\\' => array($vendorDir . '/ql/uri-template/src'),
-    'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
-    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-factory/src', $vendorDir . '/psr/http-message/src'),
-    'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
-    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src/Prophecy'),
-    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
-    'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
-    'Laminas\\Escaper\\' => array($vendorDir . '/laminas/laminas-escaper/src'),
-    'Kirby\\' => array($vendorDir . '/getkirby/kirby/src', $vendorDir . '/getkirby/composer-installer/src'),
-    'KirbyReporter\\' => array($baseDir . '/lib'),
-    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
-    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
-    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
-    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
-    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
-    '' => array($vendorDir . '/league/color-extractor/src'),
-);
+return [
+    'QL\\UriTemplate\\' => [$vendorDir.'/ql/uri-template/src'],
+    'Psr\\Http\\Message\\' => [$vendorDir.'/psr/http-factory/src', $vendorDir.'/psr/http-message/src'],
+    'Psr\\Http\\Client\\' => [$vendorDir.'/psr/http-client/src'],
+    'Kirby\\' => [$vendorDir.'/getkirby/composer-installer/src'],
+    'KirbyReporter\\' => [$baseDir.'/lib'],
+    'GuzzleHttp\\Psr7\\' => [$vendorDir.'/guzzlehttp/psr7/src'],
+    'GuzzleHttp\\Promise\\' => [$vendorDir.'/guzzlehttp/promises/src'],
+    'GuzzleHttp\\' => [$vendorDir.'/guzzlehttp/guzzle/src'],
+];
